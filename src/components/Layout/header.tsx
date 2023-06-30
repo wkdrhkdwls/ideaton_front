c4bbb00953c83d9a73d90bd7166381f2735e6871
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-06-30 17:10:42 +0900

    header 수정& tailwind 반응형 조건 수정

--- src/components/Layout/header.tsx
+++ src/components/Layout/header.tsx
@@ -9,18 +9,18 @@ const Header = () => {
   };
 
   return (
-    <nav className="border-gray-200 bg-black dark:border-gray-700">
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 xl:px-20 md:px-10">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center">
-          <span className="self-center text-white text-2xl font-semibold whitespace-nowrap ">
+        <a href="/" className="flex items-center">
+          <span className="lg:text-7xl text-2xl font-semibold whitespace-nowrap dark:text-white">
             진경옥몰
           </span>
-        </Link>{" "}
+        </a>
         <button
           onClick={toggleMenu}
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-solid-bg"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-hamburger"
           aria-expanded={isMenuOpen ? "true" : "false"}
         >
           <svg
@@ -31,86 +31,68 @@ const Header = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </button>
-        {isMenuOpen ? (
-          <div className="w-full xl:hidden">
-            <ul className="flex flex-col font-medium mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent bg-[#D3D3D3] md:dark:bg-transparent ">
-              <div className="flex flex-row  text-center">
+        <div
+          className={`w-full ${isMenuOpen ? "" : "hidden"}`}
+          id="navbar-hamburger"
+        >
+          <ul className="flex flex-col font-medium mt-4  bg-[#D3D3D3]">
+            <div className="flex flex-row  text-center">
+              <Link
+                to="/login"
+                className="w-1/2 h-14 hover:text-white hover:bg-gray-700 "
+              >
+                <button className="mt-3">로그인</button>
+              </Link>
+              <Link
+                to="/signup"
+                className="w-1/2 h-14 hover:text-white hover:bg-gray-700 "
+              >
+                <button className="mt-3">회원가입</button>
+              </Link>
+            </div>
+            <div>
+              <li>
                 <Link
-                  to="/login"
-                  className="w-1/2 h-14 hover:text-white hover:bg-gray-700 md:hover:bg-transparent"
+                  to="#"
+                  className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100  dark:hover:bg-gray-700 hover:text-white "
                 >
-                  <button className="mt-3">로그인</button>
+                  구매하기
                 </Link>
+              </li>
+              <li>
                 <Link
-                  to="/signup"
-                  className="w-1/2 h-14 hover:text-white hover:bg-gray-700 md:hover:bg-transparent"
+                  to="#"
+                  className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100  dark:hover:bg-gray-700 hover:text-white"
                 >
-                  <button className="mt-3">회원가입</button>
+                  정기배송신청하기
                 </Link>
-              </div>
-              <div>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    구매하기
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    정기배송신청하기
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    이달의원료
-                  </a>
-                </li>
+              </li>
+              <li>
+                <Link
+                  to="#"
+                  className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100  dark:hover:bg-gray-700 hover:text-white"
+                >
+                  이달의원료
+                </Link>
+              </li>
 
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    EVENT
-                  </a>
-                </li>
-              </div>
-            </ul>
-          </div>
-        ) : (
-          <ul className="hidden md:flex md:space-x-8">
-            <li>
-              <a
-                href="/login"
-                className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                로그인
-              </a>
-            </li>
-            <li>
-              <a
-                href="/signup"
-                className="block py-2 pl-3 pr-4 text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                회원가입
-              </a>
-            </li>
+              <li>
+                <Link
+                  to="#"
+                  className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100  dark:hover:bg-gray-700 hover:text-white"
+                >
+                  EVENT
+                </Link>
+              </li>
+            </div>
           </ul>
-        )}
+        </div>
       </div>
     </nav>
   );
