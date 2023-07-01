d36f5a30bb6c3eee3dbc80fbe818d1dc40569b7f
youngduck <dudejr5839@naver.com>
2023-07-01 19:05:34 +0900

    feat: 카카오 로그인

---
+++ src/components/Login/Naver/naverLogin.tsx
@@ -0,0 +1,11 @@
+import naver_login from "@assets/naver_login.png";
+
+const naverLogin = () => {
+  return (
+    <button className=" sm:w-4/5 mx-auto flex">
+      <img src={naver_login} alt="naver_login" />
+    </button>
+  );
+};
+
+export default naverLogin;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: { min: "0px", max: "350px" },
+      sm: { min: "350px", max: "720px" },
+      md: { min: "721px", max: "1080px" },
+      lg: { min: "1081px", max: "1440px" },
+      xl: { min: "1441px", max: "1920px" },
+      xxl: { min: "1921px" },
+    },
   },
   plugins: [],
 };
