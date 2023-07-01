3a5f841537c458ecbb3a1280bfac57a182a412bc
youngduck <dudejr5839@naver.com>
2023-07-01 19:58:10 +0900

    add:fullpage,jquery setting

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,35 @@
 import Layout from "@components/Layout/layout";
+import $ from "jquery";
+import "fullpage.js";
+import "fullpage.js/dist/jquery.fullpage.min.css";
 
 const Home = () => {
-  return <Layout>hi</Layout>;
+  $(() => {
+    $("#fullpage").fullpage({
+      menu: "#menu",
+      anchors: ["1", "2", "3", "4"],
+      sectionsColor: ["black", "blue", "red", "orange"],
+    });
+  });
+
+  return (
+    <Layout>
+      <div id="fullpage">
+        <div className="section" id="#section1">
+          1번
+        </div>
+        <div className="section" id="#section2">
+          2번
+        </div>
+        <div className="section" id="#section3">
+          3번
+        </div>
+        <div className="section" id="#section4">
+          4번
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
