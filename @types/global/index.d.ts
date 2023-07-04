0691d99666bc51e507dc40964e76a1f2f1146cf9
youngduck <dudejr5839@naver.com>
2023-07-04 15:28:13 +0900

    fix: option

--- @types/global/index.d.ts
+++ @types/global/index.d.ts
@@ -1,2 +1,32 @@
 declare module "*.png";
 declare module "*.jpg";
+
+declare module "*.mp4";
+// declare module "fullpage.js/dist/fullpage.min" {
+//   const fullpage: (selector: string, options: any) => any;
+//   export { fullpage };
+// }
+// declare module "fullpage.js" {
+//   type FullPageOptions = {
+//     // fullpage.js 옵션들의 타입 정의
+//     // 예시: sectionsColor?: string[]
+//     // 예시: menu?: string
+//     // ...
+//   };
+
+//   type FullPageInstance = {
+//     destroy(): unknown;
+//     // fullpage.js 인스턴스의 메소드들의 타입 정의
+//     // 예시: moveSectionUp(): void
+//     // 예시: moveTo(section: number, slide?: number): void
+//     // ...
+//   };
+
+//   type FullPageConstructor = {
+//     new (selector: string, options?: FullPageOptions): FullPageInstance;
+//   };
+
+//   const fullpage: FullPageConstructor;
+
+//   export default fullpage;
+// }
