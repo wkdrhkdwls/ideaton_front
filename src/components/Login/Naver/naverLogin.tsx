5c016d07d67696c5e6a55f3aed91c2d53693d248
youngduck <dudejr5839@naver.com>
2023-07-06 16:56:08 +0900

    add: 네이버 로그인 1

--- src/components/Login/Naver/naverLogin.tsx
+++ src/components/Login/Naver/naverLogin.tsx
@@ -1,10 +1,16 @@
 import naver_login from "@assets/login/naver_login.png";
+import { Link } from "react-router-dom";
 
 const naverLogin = () => {
+  const REACT_APP_CLIENT_ID = `${process.env.REACT_APP_CLIENT_ID}`;
+  const REACT_APP_SERVER_IP = `${process.env.REACT_APP_SERVER_IP}`;
+
   return (
-    <button className=" sm:w-4/5 mx-auto flex">
+    <Link
+      to={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${REACT_APP_CLIENT_ID}&state=JINGYEOUNGOK_NAVER_LOGIN&redirect_uri=http://${REACT_APP_SERVER_IP}:8080/login/naverLoginCallback`}
+    >
       <img src={naver_login} alt="naver_login" />
-    </button>
+    </Link>
   );
 };
 
